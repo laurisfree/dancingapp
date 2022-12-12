0000867ce763541a7dd2efe2a8b30529ee612467
laurisfree <freelau00@gmail.com>
2022-12-12 12:42:33 -0800

    sorting date

--- my-app/src/components/UserPassPurchases/UserPassPurchases.jsx
+++ my-app/src/components/UserPassPurchases/UserPassPurchases.jsx
@@ -1,19 +1,36 @@
 import './UserPassPurchases.scss';
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 
 export default function UserPassPurchases(props) {
+  const [passesBought, setpassesBought] = useState(0)
+  const [passesUsed, setpasseUsed] = useState(0)
 
-  console.log(props)
+  useEffect(() => {
+    const jwtToken = localStorage.getItem('jwt_token');
+    axios.get('http://localhost:8080/user/buy',{
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+    })
+    .then(data => {
+      setpassesBought(data.data.passesBought)
+      setpasseUsed(data.data.passesUsed)
+    })
+    .catch(error => console.log(error));
+  }, [])
+  
   return (
     <>
        <div className='user-purchases'>
             <div className='user-purchases__active-wrpr'>
               <p>ACTIVE PASSESS</p>
-              <p>{props.user?.passesBought}</p>
+              <p>{passesBought - passesUsed}</p>
             </div>
             <div className='user-purchases__used-wrpr'>
               <p>USED PASSESS</p>
-              <p>{props.user?.passesUsed}</p>
+              <p>{passesUsed}</p>
             </div>
         </div>
 
