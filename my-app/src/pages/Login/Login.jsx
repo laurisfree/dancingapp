ac7be929141d88d3b8a2940c0e3010d41850b01b
laurisfree <freelau00@gmail.com>
2022-12-12 16:01:53 -0800

    styled login

--- my-app/src/pages/Login/Login.jsx
+++ my-app/src/pages/Login/Login.jsx
@@ -1,5 +1,8 @@
 import React, {useEffect} from 'react'
 import { useNavigate } from 'react-router-dom'
+import logo from '../../assets/logo/logo-dance-studio.png'
+import './Login.scss';
+
 
 
 const Login = (props) => {
@@ -17,19 +20,24 @@ const Login = (props) => {
   
 
   return (
-    <div>
-      <h2>Home Page</h2>
+    <div className='login'>
+      {/* <video autoplay muted loop id="myVideo">
+        <source src="rain.mp4" type="video/mp4">
+      </video> */}
+      <img src={logo} alt="dance app logo" className="login__logo" />
+      <h2></h2>
       {!props.loggedIn ? (
-        <form onSubmit={props.handleLogin}>
-          <input type="email" name="email" placeholder="email" />
-          <input type="password" name="password" placeholder="password" />
-          <button type="submit">Login</button>
+        <form className='login__form' onSubmit={props.handleLogin}>
+          <input type="email" name="email" placeholder="email" className='login__input'/>
+          <input type="password" name="password" placeholder="password" className='login__input'/>
+          <button type="submit" className="login__btn">LOGIN</button>
         </form>
       ) : (
         <div>
-          <p>
-            Welcome back {props.user.name}
-          </p>
+        {/* <div className="login-left"></div> */}
+          {/* <p> */}
+            {/* Welcome back {props.user.name} */}
+          {/* </p> */}
         </div>
       )}
     </div>
