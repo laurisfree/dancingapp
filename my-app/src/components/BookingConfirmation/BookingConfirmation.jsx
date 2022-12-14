176880f5032e8d038c29b643985892df13269ed9
laurisfree <freelau00@gmail.com>
2022-12-13 17:32:19 -0800

    confirmation page styling

--- my-app/src/components/BookingConfirmation/BookingConfirmation.jsx
+++ my-app/src/components/BookingConfirmation/BookingConfirmation.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import moment from "moment";
 import axios from "axios";
+import logo from "../../assets/logo/logo-dance-studio.png"
 
 export default function BookingConfirmation() {
   const location = useLocation();
@@ -28,20 +29,24 @@ export default function BookingConfirmation() {
 
   return (
     <>
-      <div>
-        <p>You are Booking</p>
+      <div className="confirmation">
+        <div className="confirmation__wrpr-up"></div>
+        <img src={logo} alt="" className="confirmation__logo"/>
+        {/* <p>You are Booking</p> */}
       </div>
-      <div>
+      <div className="confirmation__info-wrpr">
+        <p className="confirmation__text">THANK YOU FOR BOOKING:</p>
         <p>{data.classType}</p>
         <p>{data.teacher}</p>
         <p>{moment(data.date).format("dddd MMMM Do YYYY")}</p>
         <p>{moment(data.time).format("h:mm")}</p>
       </div>
-      <div>
+      <div className="confirmation__btns-wrpr">
         {/* <Link to='/book/info'> <Link/> */}
-        <button onClick={onConfirmHanlder}>CONFIRM</button>
+        <button className="confirmation__btn-confirm" onClick={onConfirmHanlder}>CONFIRM</button>
         {/* <button>CONFIRM</button> */}
         {/* <p>Add to my Caledar</p> */}
+        <div className="confirmation__btn-calendar">
         <a
           href={`https://calendar.google.com/calendar/render?action=TEMPLATE&text=${
             data.classType
@@ -49,9 +54,11 @@ export default function BookingConfirmation() {
             "dddd MMMM Do YYYY"
           )}&details=${"description"}&location=${"Vancover"}`}
           target="_blank"
+          className="confirmation__btn-calendar-a"
         >
-          add to calendar
+          ADD TO CALENDAR
         </a>
+        </div>
         {/* console.log(Date.now()); */}
         {/* console.log(moment(data.time).format('h:mm')) */}
       </div>
