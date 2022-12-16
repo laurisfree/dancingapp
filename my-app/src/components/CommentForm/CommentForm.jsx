32cc7533df99f12ef21e0302afc4811026de753f
laurisfree <freelau00@gmail.com>
2022-12-16 11:51:34 -0800

    fixing header and footer and user purchases mobile

---
+++ my-app/src/components/CommentForm/CommentForm.jsx
@@ -0,0 +1,18 @@
+import './CommentForm.scss'
+
+
+export default function Dancer (){
+    return (
+        <>
+        <div className='comments'>
+            <p>Dancer Log</p>
+        </div>
+        <div>
+            <p>This space is for you to track your progress or leave notes for your next class.</p>
+            <p> Keep a record of your photos, add reminders of the Choreos you have learnt</p>
+            
+        </div>
+        
+        </>
+    )
+}
--- my-app/src/pages/Dancer/Dancer.jsx
+++ my-app/src/pages/Dancer/Dancer.jsx
@@ -1,11 +1,13 @@
 import './Dancer.scss'
 import Header from '../../components/Header/Header';
+import CommentForm from '../../components/CommentForm/CommentForm';
 
 export default function Dancer (){
     return (
         <>
         
         <Header />
+        <CommentForm />
      
         
         
